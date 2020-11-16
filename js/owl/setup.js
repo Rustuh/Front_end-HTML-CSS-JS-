8f579a3086db3868667ffc1de8cb5023eecc5585
unknown <you@example.com>
2020-11-16 18:20:44 -0300

    first commit

---
+++ js/owl/setup.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    items:1,
+    merge:true,
+    loop:true,
+    margin:10,
+    center:true,
+    responsive:{
+        480:{
+            items:2
+        },
+        600:{
+            items:4
+        }
+    }
+})
